094467a3030ba50aaba361bec705051e11c60b48
ellacatev <89489187+ellacatev@users.noreply.github.com>
2021-08-30 15:43:19 -0700

    I made my crab turn at the edge and spin. I learned about booleans and voids.

--- Crab.java
+++ Crab.java
@@ -7,8 +7,39 @@ public class Crab extends Actor
 {
     public void act()
     {
-        // Add your action code here
+                move(-3);
+         turnAtEdge();
+         checkKeyPress();
+         onCollision();
+
+    }//This method repeats the following actions public void act()
+    
+    //Moves the Crab 
+    
+    //Turns the Crab at the edge 
+    private void turnAtEdge()
+    {
+        if(isAtEdge())
+        {
+            turn(-50);
+        }
     }
-}
+
+    //Checks for user key presses so user can turn the Crab 
+    private void checkKeyPress()
+    {
+        
+    }
+    //Checks for collisions with other objects 
+    private void onCollision()
+    {
+    }
+    }
+    
+    
+    
+    
+    
+
 
 
