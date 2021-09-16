7897e56026c88918a6fe2aca84894f572472d6e0
ellacatev <89489187+ellacatev@users.noreply.github.com>
2021-09-16 14:14:29 -0700

    fixed!

--- snow.java
+++ snow.java
@@ -17,5 +17,19 @@ public class snow extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        fish fish = new fish();
+        addObject(fish,201,162);
+        polarbear polarbear = new polarbear();
+        addObject(polarbear,478,206);
+        seal seal = new seal();
+        addObject(seal,326,67);
     }
 }
